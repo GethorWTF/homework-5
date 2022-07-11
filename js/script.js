ee4ad87dd593427e3125034b95e5d8618628f9b7
GethorWTF <94431044+GethorWTF@users.noreply.github.com>
2022-07-11 15:44:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// 1 задача
+
+  const twiceAsOld = (dadYearsOld, sonYearsOld) => 
+  Math.abs(sonYearsOld * 2 - dadYearsOld)
+  console.log(twiceAsOld(55, 30));
+
+// 2 задача
+
+const oddCount = n => Math.floor(n/2)
+console.log(oddCount(15));
+
+// 3 задача
+
+const stringToNumber = str => Number(str)
